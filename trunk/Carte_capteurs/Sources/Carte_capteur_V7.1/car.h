340b8eb7846a359c670f9fb4c96345f7e452030a
michael.brogniaux <michael.brogniaux@6b799d73-1397-7c61-d390-6d97dc60fa6c>
2013-04-16 07:43:05 +0000

    Code source carte capteur v7.1

---
+++ trunk/Carte_capteurs/Sources/Carte_capteur_V7.1/car.h
@@ -0,0 +1,39 @@
+/* car.h */
+
+/**********************************************************************
+*
+* File name		 : car.h
+* Title 		 : Caractères et messages
+* Author 		 : Michaël Brogniaux - Copyright (C) 2013
+* Created		 : 03/02/2013
+* Last revised	 : 18/03/2013
+* Version		 : 1.1.4
+* Compliler		 : AVR Studio 4.18.716 - WinAVR-20100110
+* MCU			 : Atmel ATmega88
+*
+**********************************************************************/
+
+#ifndef car_H
+#define car_H
+
+/**********************************************************************/
+#define DEG_CAR				167 	// Symbole "degre" : °
+#define NEG_CAR				'-'		// Symbole "moins" : -
+#define C_CAR				'C'		// Lettre : C
+#define COMMA_CAR			','		// Virgule
+#define DOT_CAR				'.'		// Point
+#define SPACE_CAR 			' '		// Espace
+#define PERCENT_CAR 		'%'		// Pourcent
+#define NEW_PAGE		 	'\f'	// Nouvelle page
+#define NEW_LINE		 	'\n'	// Retour à la ligne
+#define CARRIAGE_RETURN 	'\r'	// Retour à la ligne
+#define	BELL				'\a'	// Sonnerie
+#define CR_NL				""		// Sans caractère (pour CR et NL)
+#define MSG_WELCOME			" /== Welcome in the I2C bus tester software ==\\ "
+#define MSG_YEAR			" \\== MA2EN 2012-2013  Hrd v1.3b - Sft v1.2.10 ==/"
+#define ERROR_MSG			" connection error !!!"
+#define	CONN_MSG			" connected."
+#define STX					0x02	// Start of header
+/**********************************************************************/
+
+#endif
